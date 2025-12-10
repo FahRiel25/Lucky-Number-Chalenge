e832f78e71c7ff77dabc170ea7dc0e25125ec8ad
deariftyaa <sitiasiyah190783@gmail.com>
2025-12-10 12:00:41 +0700

    menambahkan background di halaman profile dan history

--- app/src/main/java/com/example/luckynumberchalenge/HistoryAdapter.kt
+++ app/src/main/java/com/example/luckynumberchalenge/HistoryAdapter.kt
@@ -7,20 +7,20 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class HistoryAdapter (val historyData: List<History>): RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
+class HistoryAdapter (val data: List<History>): RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup, viewType: Int
     ): HistoryViewHolder {
         val layout = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_history, parent, false)
-
         return HistoryViewHolder(layout)
     }
 
     override fun onBindViewHolder(
-        holder: HistoryViewHolder, position: Int,
+        holder: HistoryViewHolder,
+        position: Int,
     ) {
-        val history: History = historyData[position]
+        val history: History = data[position]
         holder.textViewCircle.text = history.angka.toString()
         holder.textViewAngka.text = "Angka: ${history.angka}"
         holder.textViewHukuman.text = "Hukuman: ${history.hukuman}"
@@ -28,8 +28,8 @@ class HistoryAdapter (val historyData: List<History>): RecyclerView.Adapter<Hist
         holder.iconTime.setImageResource(R.drawable.icon_history)
     }
 
-    override fun getItemCount(): Int = historyData.size
-    class HistoryViewHolder(row: View): RecyclerView.ViewHolder(row){
+    override fun getItemCount(): Int = data.size
+    class HistoryViewHolder(val row: View): RecyclerView.ViewHolder(row){
         val textViewCircle: TextView = row.findViewById<TextView>(R.id.textViewCircle)
         val textViewAngka: TextView = row.findViewById<TextView>(R.id.textViewAngka)
         val textViewHukuman: TextView = row.findViewById<TextView>(R.id.textViewHukuman)
--- app/src/main/java/com/example/luckynumberchalenge/HistoryFragment.kt
+++ app/src/main/java/com/example/luckynumberchalenge/HistoryFragment.kt
@@ -10,37 +10,61 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class HistoryFragment : Fragment() {
-    private lateinit var rvHistory: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_history, container, false)
-        rvHistory = view.findViewById(R.id.rvHistory)
-        val historyList = createDummyHistory()
+        val rvHistory = view.findViewById<RecyclerView>(R.id.rvHistory)
+        val historyList = getHistory()
         val adapter = HistoryAdapter(historyList)
         rvHistory.adapter = adapter
         rvHistory.layoutManager = LinearLayoutManager(requireContext())
         return view
     }
 
-    fun createDummyHistory(): List<History> {
-        return listOf(
+    fun getHistory(): List<History> {
+        val data = mutableListOf<History>()
+        data.add(
             History(
                 angka = 7,
                 hukuman = "Push up 20 kali",
                 tanggal = "15 Januari 2025, 15.30"
-            ),
+            )
+        )
+
+        data.add(
             History(
                 angka = 3,
                 hukuman = "Upload foto selfie terjelek",
-                tanggal = "15 Januari 2025, 15.30"
-            ),
+                tanggal = "13 Januari 2025, 09.30"
+            )
+        )
+
+        data.add(
             History(
                 angka = 1,
                 hukuman = "Traktir temen es krim",
-                tanggal = "15 Januari 2025, 15.30"
+                tanggal = "15 Januari 2025, 14.30"
             )
         )
+
+        data.add(
+            History(
+                angka = 2,
+                hukuman = "Makan cabai",
+                tanggal = "24 Januari 2025, 18.30"
+            )
+        )
+
+        data.add(
+            History(
+                angka = 3,
+                hukuman = "Traktir gacoan",
+                tanggal = "22 Februari 2025, 15.30"
+            )
+        )
+        return data
     }
 }
